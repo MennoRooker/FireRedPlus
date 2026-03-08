2f9c4ff077dd5d37268d69df6d7401ef48462a71
MennoRooker <mennorooker@hotmail.com>
2026-03-09 00:23:19 +0100

    CableClub broke pokecenters

--- include/constants/flags.h
+++ include/constants/flags.h
@@ -195,10 +195,10 @@
 #define FLAG_BUGSY_ITEM_EXCHANGED                          0x0B3
 #define FLAG_HIDE_TEAM_ROCKET_ROUTE7                       0x0B4
 #define FLAG_HIDE_SWIMMER_CERULEAN_CITY                    0x0B5
+#define FLAG_USED_PKMNCENTER_ONCE                          0x0B6
 
 
 // Unused
-#define FLAG_0x0B6               0x0B6
 #define FLAG_0x0B7               0x0B7
 #define FLAG_0x0B8               0x0B8
 #define FLAG_0x0B9               0x0B9
